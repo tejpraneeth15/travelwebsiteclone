153cd769aac92d72385f82e4642e06252f2280a2
tejpraneeth15 <tejpraneethpalukuri@gmail.com>
2025-02-13 17:32:10 +0530

    added error.ejs file

--- app.js
+++ app.js
@@ -128,7 +128,8 @@ app.all("*",(req,res,next)=>{
 
 app.use((err,req,res,next)=>{
     let {statusCode=500,message="Something went wrong"}=err;
-    res.status(statusCode).send(message);
+    res.status(statusCode).render("listings/error.ejs",{message});
+    //res.status(statusCode).send(message);
     })
 
 app.listen(5000,()=>{
